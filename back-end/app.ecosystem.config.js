345381861f87bb9f57fd277c97bb3c32bbd26b74
Nithin <nithin@inncircles.com>
2024-10-20 10:19:42 +0530

    feat: pm2-config

---
+++ back-end/app.ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [{
+        name: "app",
+        script: "app.js",
+        instances: -1,
+        exec_mode: "cluster",
+        node_args: "--max-old-space-size=4096"
+    }]
+}
